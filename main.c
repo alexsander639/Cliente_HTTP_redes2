d906a50ef42dcd62ef1b9572ff5b2665b35f6768
alexsander639 <alexsander639@gmail.com>
2023-05-24 14:27:38 -0300

    Criação do arquivo main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+/*	Redes II - Cliente HTTP
+    Aluno: Alex Sander S. A. Junior		RGA: 2019.0743.005-9
+    Aluno: Vinícius Silva		        RGA: 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
